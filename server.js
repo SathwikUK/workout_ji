2ece139d91601007cd00b4659a7d043fcf5db6ac
SathwikUK <sathwiksampengala@gmail.com>
2024-07-07 18:58:22 +0530

    done

--- server.js
+++ server.js
@@ -1,49 +1,49 @@
-const path = require('path')
-const fileURLToPath = require('url')
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const workoutRoutes = require('./routes/workouts')
-const UserRoutes = require('./routes/user')
-//express app
-const app = express()
+const path = require('path');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user');
 
+// Express app
+const app = express();
 
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = path.dirname(__filename)
-
-app.use(express.json())
+app.use(express.json());
 
 app.use(cors({
     origin: 'http://localhost:3000', // Allow requests from this origin
     credentials: true, // Allow credentials (e.g. cookies) to be sent with the request
-  }));
+}));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    console.log(req.path, req.method)
+    console.log(req.path, req.method);
     next();
-  });
+});
 
-app.use('/api/workouts' ,workoutRoutes)
-app.use('/api/user' ,UserRoutes)
+// API routes
+app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes);
 
-app.use(express.static(path.join(__dirname ,'/client/build')))
-app.get('*',(req ,res) => {
-    res.sendFile(path.join(__dirname ,"/client/build/index.html"))
-}) 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
 
+// Connect to MongoDB and start the server
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => {
+        console.log(`connected to db and listening to port ${PORT}`);
+    });
+}).catch((error) => {
+    console.log(error);
+});
 
-mongoose.connect(process.env.MONGODB_URI).then(()=>{
-    app.listen(process.env.PORT ,()=>{
-        console.log("connected to db and listening to port 4000")
-    })
-}).catch((error)=>{
-    console.log(error)
-})
-app.get('/' ,(req ,res) =>{
-    res.json({messg:'welcome to app'})
-})
+app.get('/', (req, res) => {
+    res.json({ message: 'welcome to app' });
+});
